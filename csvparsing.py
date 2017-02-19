b8609fcee755983a93fc488a6495e417e44b522d
Kenneth Bambridge <kbambridge1@gmail.com>
2017-02-19 09:22:00 -0500

    Add csvparsing.py for aggregating data

---
+++ csvparsing.py
@@ -0,0 +1,8 @@
+# Aggregate each heuristic across all algorithms
+# Aggregate each heuristic modifier across a_star
+# Aggregate each algorithm across all heuristics
+# Aggregate each anchor heuristic for a_star_sequential a_star_integrated
+#
+# These can be done in excel by duplicating the sheet and sorting/aggregating:
+# Fix w1, increase w2 as function of running time
+# Fix w2, increase w1 as functino of running time
